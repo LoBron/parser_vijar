513f009e4978a57e5f0870aeabd44dd5bcb11ac7
LoBron <lobanok2793@gmail.com>
2022-04-22 15:38:33 +0300

    add threading, refactoring

--- parser.py
+++ parser.py
@@ -1,7 +1,5 @@
-
-import requests
 from bs4 import BeautifulSoup as BS
-from utils import DataProducts, download_img
+from utils import *
 
 data_products = DataProducts('catalog_vijar')
 name_category = 'metiznaya_produktsiya'
@@ -22,61 +20,18 @@ while True:
             product_detail = requests.get(href_product)
             html_item = BS(product_detail.content, 'html.parser')
 
-            name = html_item.select('.product_name > h1 > b')[0].text.strip()
-            object_data['name'] = name
+            object_data['name'] = get_name(html_item)
 
-            price = float(html_item.select('span.price')[0].text.strip())
-            object_data['price'] = price
+            object_data['price'] = get_price(html_item)
 
-            properties = {}
-            prop = html_item.select('div.charakters > ul.properties > li')
-            n = 1
-            for property in prop:
-                if n <= len(prop)/2:
-                    property = property.text.split(': ')
-                    properties[property[0]] = property[1]
-                    n += 1
-                else:
-                    break
-            object_data['properties'] = properties
+            object_data['properties'] = get_properties(html_item)
 
-            def get_photo(name, n, html_item, path):
+            object_data['photos'] = get_photos(object_data['name'], html_item)
 
-                if n == 1:
-                    photo = html_item.find_all('img', alt=name)
-                else:
-                    photo = html_item.find_all('img', alt=name+' — фото'+str(n))
-                photo_url = f"https://viyar.ua{photo[0].get('src')}"
-
-                photos[f"photo{n}"] = photo_url
-                root = f"{path}\{photo_url.split('/')[-1]}"
-                download_img(photo_url, root)
-                print(f'фото №{n} загружено')
-                return
-
-            photos = {}
-            for n in range(1, 5):
-                try:
-                    get_photo(name, n, html_item, path)
-                except IndexError:
-                    break
-                # try:
-                #     if n == 1:
-                #         photo = html_item.find_all('img', alt=name)
-                #     else:
-                #         photo = html_item.find_all('img', alt=name+' — фото'+str(n))
-                #     photo_url = f"https://viyar.ua{photo[0].get('src')}"
-                # except IndexError:
-                #     break
-                # photos[f"photo{n}"] = photo_url
-                # root = f"{path}\{photo_url.split('/')[-1]}"
-                # download_img(photo_url, root)
-
-            object_data['photos'] = photos
+            download_photos(object_data.get('photos'), path)
 
             data.append(object_data)
-            print(object_data)
-            print(f"object '{name}' added")
+            print(f"object '{object_data['name']}' added")
 
         print(f"page {page} added\n")
         page += 1
@@ -84,5 +39,5 @@ while True:
         break
 
 data_products.add_data(name_category, data)
-print(data_products.get_data())
+# print(data_products.get_data())
 
--- utils.py
+++ utils.py
@@ -1,13 +1,5 @@
+import threading
 import requests
-import time
-from threading import *
-
-
-def download_img(url, root):
-    p = requests.get(url)
-    img = open(root, "wb")
-    img.write(p.content)
-    img.close()
 
 class DataProducts:
     def __init__(self, name):
@@ -23,6 +15,58 @@ class DataProducts:
         else:
             return self.data
 
-def get_data(name, data):
-    print(f"{name} - {data}")
-    time.sleep(5)
+def get_name(html_item):
+    """Возвращает наименование товара"""
+    name = html_item.select('.product_name > h1 > b')[0].text.strip()
+    return name
+
+def get_price(html_item):
+    """Возвращает стоимость товара за единицу"""
+    price = float(html_item.select('span.price')[0].text.strip())
+    return price
+
+def get_properties(html_item):
+    """Возвращает характеристики товара"""
+    properties = {}
+    prop_list = html_item.select('div.charakters > ul.properties > li')
+    n = 1
+    for prop in prop_list:
+        if n <= len(prop_list) / 2:
+            property = prop.text.split(': ')
+            properties[property[0]] = property[1]
+            n += 1
+        else:
+            break
+    return properties
+
+def get_photos(name, html_item):
+    """Возвращает url адреса фотографий"""
+    photos = []
+    for n in range(1, 5):
+        try:
+            if n == 1:
+                photo = html_item.find_all('img', alt=name)
+            else:
+                photo = html_item.find_all('img', alt=name + ' — фото' + str(n))
+            photo_url = f"https://viyar.ua{photo[0].get('src')}"
+            photos.append(photo_url)
+        except IndexError:
+            break
+    return photos
+
+def download_photos(kwargs, path):
+    """Запускает потоки и загружает в них фотографии"""
+    if kwargs:
+        n = 1
+        for img_url in kwargs:
+            threading.Thread(target=download_img, args=(img_url, path, n), name=f'thr-{n}').start()
+            n += 1
+
+def download_img(url, path, n):
+    """Загружает фотографию"""
+    root = path + '/' + url.split('/')[-1]
+    p = requests.get(url)
+    img = open(root, "wb")
+    img.write(p.content)
+    img.close()
+    print(f"Загружено фото {n}")
