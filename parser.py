e17fd2924fa6523fa1d74445d5c88a32e5981790
LoBron <lobanok2793@gmail.com>
2022-04-25 17:39:58 +0300

    changed the algorithm (version 2)

--- parser.py
+++ parser.py
@@ -1,28 +1,71 @@
+import random
+
 from utils import *
+import multiprocessing
 import time
 
-data_products = DataProducts('catalog_vijar')
+# data_products = DataProducts('catalog_vijar')
 name_category = 'metiznaya_produktsiya'
 path = 'products_data/'
 data = []
 page = 1
+timer = time.time()
+pages = 12 #12
+amount_processor_cores = 4
+########################################################################################################################
+def get_category_page(name_category, page):
+    return requests.get(f'https://viyar.ua/catalog/{name_category}/page-' + str(page))
 
-while page <= 10:
-    category_list = requests.get(f'https://viyar.ua/catalog/{name_category}/page-' + str(page))
-    html = BS(category_list.content, 'html.parser')
-    items = html.select('.product_prewiew')
-    if len(items):
-        start = time.time()
+def get_category_pages(name_category, amount_processor_cores):
+    response_page_list = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=amount_processor_cores * 2) as executor:
+        future_list = [executor.submit(get_category_page, name_category, page) for page in range(1, pages + 1)]
+        for future in concurrent.futures.as_completed(future_list):
+            response_page_list.append(future.result())
+    return response_page_list
 
-        data += add_page_data(items, path)
-        print(data)
 
-        print(f"page {page} added\n")
-        page += 1
-        print(f"Время выполнения - {time.time()-start}")
-    else:
-        break
+timer_0 = time.time()
+response_page_list = get_category_pages(name_category, amount_processor_cores)
+print('список страниц добавлен')
+print(f"0) время выполнения - {time.time() - timer_0}\n")
+########################################################################################################################
 
-data_products.add_data(name_category, data)
-# print(data_products.get_data())
+# sequence = 6, 8, 10, 12
+# d = {f'{i} процессов': [] for i in sequence}
+# for i in range(40):
+#     timer_1 = time.time()
+#     n = random.choice(sequence)
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=number_processor_cores*2) as executor:
+#         future_list = [executor.submit(get_category_page, name_category, page) for page in range(1, pages+1)]
+#         for future in concurrent.futures.as_completed(future_list):
+#             response_page_list.append(future.result())
+#         d[f'{n} процессов'].append(time.time()-timer_1)
+#         print(f"{i} Итерация, время выполнения - {time.time() - timer_1}")
+#
+# # d = {'sdfdsfsd': [121, 2323, 433, 5445, 45]}
+# for j in d.items():
+#     print(f'{j[0]} -- вызовов: {len(j[1])} шт -- среднее значение: {sum(j[1])/len(j[1])} сек')
 
+########################################################################################################################
+
+timer_1 = time.time()
+items_list = get_items_hrefs(response_page_list)
+print(items_list)
+print(f"1) время выполнения - {time.time() - timer_1}\n")
+########################################################################################################################
+
+timer_2 = time.time()
+response_items = get_items_pages(items_list, amount_processor_cores)
+print(response_items)
+print(f"2) время выполнения - {time.time() - timer_2}\n")
+
+########################################################################################################################
+
+timer_3 = time.time()
+data = add_items_data(response_items, path)
+print(data)
+print(f"3) время выполнения - {time.time() - timer_3}\n")
+
+########################################################################################################################
+print(f"время скачивания данных - {time.time() - timer}")
--- utils.py
+++ utils.py
@@ -5,47 +5,90 @@ import concurrent.futures
 import requests
 from bs4 import BeautifulSoup as BS
 
+def get_items_hrefs(response_page_list):
+    items_hrefs = []
+    for response_page in response_page_list:
+        html = BS(response_page.content, 'html.parser')
+        items = html.select('.product_prewiew')
+        if len(items):
+            for item in items:
+                product = item.select('a')
+                items_hrefs.append(f"https://viyar.ua{product[0].get('href')}")
+    return items_hrefs
 
-
-
-class DataProducts:
-    def __init__(self, name):
-        self.name = name
-        self.data = {}
-
-    def add_data(self, name_category, data):
-        self.data[name_category] = data
-
-    def get_data(self, name_category=None):
-        if name_category:
-            return self.data.get(name_category)
-        else:
-            return self.data
-
-def add_page_data(items, path):
+def add_items_data(response_items, path):
     """https://docs.python.org/3.8/library/concurrent.futures.html#threadpoolexecutor"""
-    page = []
+    items_data = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_item = [executor.submit(add_item_data, item, path) for item in items]
-        for future in concurrent.futures.as_completed(future_to_item):
-            page.append(future.result())
-    return page
+        future_list = [executor.submit(add_item_data, response_item, path) for response_item in response_items]
+        for future in concurrent.futures.as_completed(future_list):
+            items_data.append(future.result())
+    return items_data
 
-def add_item_data(item, path):
+def add_item_data(response_item, path):
     """Возвращает данные о товаре и загружает его фотографии"""
-    object_data = {}
+    item_data = {}
+    item = BS(response_item.content, 'html.parser')
+    item_data['name'] = get_name(item)
+    item_data['price'] = get_price(item)
+    item_data['properties'] = get_properties(item)
+    item_data['photos'] = get_photos(item_data['name'], item)
+    download_photos(item_data.get('photos'), path)
+    # print(f"object '{object_data['name']}' added")
+    return item_data
 
-    product = item.select('a')
-    href_product = f"https://viyar.ua{product[0].get('href')}"
-    product_detail = requests.get(href_product)
-    html_item = BS(product_detail.content, 'html.parser')
-    object_data['name'] = get_name(html_item)
-    object_data['price'] = get_price(html_item)
-    object_data['properties'] = get_properties(html_item)
-    object_data['photos'] = get_photos(object_data['name'], html_item)
-    download_photos(object_data.get('photos'), path)
-    print(f"object '{object_data['name']}' added")
-    return object_data
+
+def get_item_page(item_href):
+    return requests.get(item_href)
+
+def get_items_pages(items_list, amount_processor_cores):
+    items_pages = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=amount_processor_cores*2) as executor:
+        future_list = [executor.submit(get_item_page, item) for item in items_list]
+        for future in concurrent.futures.as_completed(future_list):
+            items_pages.append(future.result())
+    return items_pages
+
+
+
+# class DataProducts:
+#     def __init__(self, name):
+#         self.name = name
+#         self.data = {}
+#
+#     def add_data(self, name_category, data):
+#         self.data[name_category] = data
+#
+#     def get_data(self, name_category=None):
+#         if name_category:
+#             return self.data.get(name_category)
+#         else:
+#             return self.data
+
+# def add_page_data(items, path):
+#     """https://docs.python.org/3.8/library/concurrent.futures.html#threadpoolexecutor"""
+#     page = []
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+#         future_to_item = [executor.submit(add_item_data, item, path) for item in items]
+#         for future in concurrent.futures.as_completed(future_to_item):
+#             page.append(future.result())
+#     return page
+
+# def add_item_data(item, path):
+#     """Возвращает данные о товаре и загружает его фотографии"""
+#     object_data = {}
+#
+#     product = item.select('a')
+#     href_product = f"https://viyar.ua{product[0].get('href')}"
+#     product_detail = requests.get(href_product)
+#     html_item = BS(product_detail.content, 'html.parser')
+#     object_data['name'] = get_name(html_item)
+#     object_data['price'] = get_price(html_item)
+#     object_data['properties'] = get_properties(html_item)
+#     object_data['photos'] = get_photos(object_data['name'], html_item)
+#     download_photos(object_data.get('photos'), path)
+#     # print(f"object '{object_data['name']}' added")
+#     return object_data
 
 def get_name(html_item):
     """Возвращает наименование товара"""
@@ -89,16 +132,17 @@ def get_photos(name, html_item):
 def download_photos(kwargs, path):
     """Запускает потоки и загружает в них фотографии"""
     if kwargs:
-        n = 1
+        # n = 1
         for img_url in kwargs:
-            threading.Thread(target=download_img, args=(img_url, path, n), name=f'thr-{n}').start()
-            n += 1
+            threading.Thread(target=download_img, args=(img_url, path)).start()
+            # threading.Thread(target=download_img, args=(img_url, path, n), name=f'thr-{n}').start()
+            # n += 1
 
-def download_img(url, path, n):
+def download_img(url, path):
     """Загружает фотографию"""
     root = path + url.split('/')[-1]
     p = requests.get(url)
     img = open(root, "wb")
     img.write(p.content)
     img.close()
-    print(f"Загружено фото {n}")
+    # print(f"Загружено фото {n}")
