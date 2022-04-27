b005714d3910c11bcc7d6e5e98c8825aef357416
LoBron <lobanok2793@gmail.com>
2022-04-27 23:03:45 +0300

    big rework

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import random
 import threading
 import time
 import concurrent.futures
@@ -5,19 +6,99 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 
-def get_category_page(name_category, page):
-    return requests.get(f'https://viyar.ua/catalog/{name_category}/page-' + str(page))
 
-def get_category_pages(name_category, pages):
+def get_categories_info(main_url):
+    categories_info = []
+    response_obj = requests.get(main_url)
+    html_page = BS(response_obj.content, 'html.parser')
+    items0_list = html_page.select('.main-menu > .top_level > li')
+    for item0 in items0_list:
+        cat0_name = item0.find_all('span')[0].text.strip()
+        if cat0_name in ['Фасады', 'Фасади']:
+            continue
+        else:
+            cat_level_0 = {}
+            cat_level_0["name"] = cat0_name
+            cat_level_0["slug"] = 'надо доработать'
+            cat_level_0["categories_level_1"] = []
+            items1_list = item0.select('.hidden-label > div')
+            for item1 in items1_list:
+                if len(item1) > 0:
+                    links = item1.find_all('a')
+                    cat1_name = links[0].text.strip()
+                    if cat1_name in ['Мойки из искусственного камня  Belterno', 'Мийки зі штучного каменю  Belterno']:
+                        continue
+                    else:
+                        cat_level_1 = {}
+                        cat_level_1["name"] = cat1_name  #links[0]["title"]
+                        cat_level_1["href"] = links[0]["href"]
+                        cat_level_1["slug"] = cat_level_1["href"].split('/')[-2]
+                        cat_level_1["categories_level_2"] = []
+                        if len(links) > 1:
+                            for n in range(1, len(links)):
+                                cat_level_2 = {}
+                                cat_level_2["name"] = links[n].text.strip()
+                                cat_level_2["href"] = links[n]["href"]
+                                cat_level_2["slug"] = cat_level_2["href"].split('/')[-2]
+                                cat_level_1["categories_level_2"].append(cat_level_2)
+                        cat_level_0["categories_level_1"].append(cat_level_1)
+            categories_info.append(cat_level_0)
+    return categories_info
+
+
+def add_products_data(data, main_url, path):
+    for i in range(1): #range(len(data)):
+        print(data[i])
+        for j in range(len(data[i].get("categories_level_1"))):
+            print(data[i].get("categories_level_1"))
+            if len(data[i].get("categories_level_1")[j].get("categories_level_2")) > 0:
+                for k in range(len(data[i].get("categories_level_1")[j].get("categories_level_2"))):
+                    url = main_url + data[i].get("categories_level_1")[j].get("categories_level_2")[k]['href']
+                    data[i].get("categories_level_1")[j].get("categories_level_2")[k]['products'] = get_category_data(url=url, path=path)
+            else:
+                url = main_url + data[i].get("categories_level_1")[j]['href']
+                data[i].get("categories_level_1")[j]['products'] = get_category_data(url=url, path=path)
+
+
+
+def get_category_data(url, path):
+    response_page_list = get_category_pages(url=url)
+    print('получили страницы')
+    print(response_page_list)
+    items_list = get_items_hrefs(response_page_list)
+    print('получили urls обьектов')
+    print(items_list)
+    response_items = get_items_pages(items_list)
+    print('получили страницы обьектов')
+    # print(response_items)
+    return add_items_data(response_items, path)
+
+
+
+def get_category_pages(url):
     response_page_list = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=pages) as executor:
-        timer_0 = time.time()
-        future_list = [executor.submit(get_category_page, name_category, page) for page in range(1, pages + 1)]
-        for future in concurrent.futures.as_completed(future_list):
-            response_page_list.append(future.result())
-            print(f"1) время выполнения - {time.time() - timer_0}")
-        print(f"1) время выполнения ФУНКЦИИ - {time.time() - timer_0}\n")
+    n = 1
+    while True:
+        response_page = requests.get(f'{url}page-{n}/')
+        if response_page.status_code == 200: #in range(200, 207):
+            response_page_list.append(response_page)
+            print(f'страница {n} добавлена')
+            n += 1
+        else:
+            break
     return response_page_list
+
+
+# def get_category_pages(name_category, pages):
+#     response_page_list = []
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=pages) as executor:
+#         timer_0 = time.time()
+#         future_list = [executor.submit(get_category_page, name_category, page) for page in range(1, pages + 1)]
+#         for future in concurrent.futures.as_completed(future_list):
+#             response_page_list.append(future.result())
+#             print(f"1) время выполнения - {time.time() - timer_0}")
+#         print(f"1) время выполнения ФУНКЦИИ - {time.time() - timer_0}\n")
+#     return response_page_list
 
 def get_items_hrefs(response_page_list):
     items_hrefs = []
@@ -35,7 +116,7 @@ def get_item_page(item_href):
 
 def get_items_pages(items_list):
     items_pages = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
         future_list = [executor.submit(get_item_page, item) for item in items_list]
         for future in concurrent.futures.as_completed(future_list):
             items_pages.append(future.result())
@@ -46,22 +127,29 @@ def add_item_data(response_item, path):
     item_data = {}
     item = BS(response_item.content, 'html.parser')
     item_data['name'] = get_name(item)
+    print(f"object '{item_data['name']}' added")
     item_data['slug'] = get_slug(response_item)
     item_data['price'] = get_price(item)
     item_data['properties'] = get_properties(item)
     item_data['photos'] = get_photos(item_data['name'], item)
     download_photos(item_data.get('photos'), path)
-    # print(f"object '{object_data['name']}' added")
+    # print(f"object '{item_data['name']}' added")
     return item_data
 
+# def add_items_data(response_items, path):
+#     """https://docs.python.org/3.8/library/concurrent.futures.html#threadpoolexecutor"""
+#     items_data = []
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+#         future_list = [executor.submit(add_item_data, response_item, path) for response_item in response_items]
+#         for future in concurrent.futures.as_completed(future_list):
+#             items_data.append(future.result())
+#     return items_data
 def add_items_data(response_items, path):
-    """https://docs.python.org/3.8/library/concurrent.futures.html#threadpoolexecutor"""
     items_data = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_list = [executor.submit(add_item_data, response_item, path) for response_item in response_items]
-        for future in concurrent.futures.as_completed(future_list):
-            items_data.append(future.result())
+    for response_item in response_items:
+        items_data.append(add_item_data(response_item, path))
     return items_data
+
 
 def get_name(html_item):
     """Возвращает наименование товара"""
@@ -72,15 +160,25 @@ def get_slug(response_item):
     url = response_item.url
     return url.split('/')[-2]
 
+
 def get_price(html_item):
     """Возвращает стоимость товара за единицу"""
     try:
+        # print(html_item.select('span.price'))
         price = float(html_item.select('span.price')[0].text.strip())
+    except IndexError:
+        return random.choice(range(1700, 2500, 3))
     except ValueError:
         price_l = html_item.select('span.price')[0].text.strip().split()
         return float(price_l[0]+price_l[1])
     else:
         return price
+
+
+
+
+
+
 
 def get_properties(html_item):
     """Возвращает характеристики товара"""
@@ -90,7 +188,7 @@ def get_properties(html_item):
     for prop in prop_list:
         if n <= len(prop_list) / 2:
             property = prop.text.split(': ')
-            properties[property[0]] = property[1]
+            properties[property[0]] = property[1].lower()
             n += 1
         else:
             break
