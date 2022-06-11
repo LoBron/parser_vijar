16bd53851e761f51b75e734bb32ffee2dc8ee57f
LoBron <lobanok2793@gmail.com>
2022-06-11 08:37:55 +0300

    save none.py end add requairments.txt

--- none.py
+++ none.py
@@ -29,11 +29,16 @@ import time
 
 
 async def get_response_page(category_url, number_page, session):
-    async with session.get(f'{category_url}page-{number_page}/') as response:
+    # try:
+    print('aaaaaaaaaaaaaaaaaaaaaaa')
+    async with session.get(f'{category_url}page-{number_page}') as response:
+        print('bbbbbbbbbbbbbbbbbbbbbbbbb')
         return await response.text()
+    # except Exception as ex:
+    #     print(ex)
 
 
-async def get_response_pages(category_url):
+async def get_response_pages(category_url, session):
     """Возвращает список response обьектов со страницами пагинатора внутри категории"""
     response_page_list = []
     response_page = requests.get(f'{category_url}')
@@ -44,12 +49,14 @@ async def get_response_pages(category_url):
         return response_page_list
     else:
         n = int(paggination_list[-2].text)
-        async with aiohttp.ClientSession() as session:
-            task_list = [asyncio.create_task(get_response_page(category_url, i, session)) for i in
-                         range(2, n + 1)]  # (2, n + 1)]
-            for future in asyncio.as_completed(task_list):
-                response_page_list.append(await future)
-        await asyncio.sleep(0.1)
+
+        task_list = [asyncio.create_task(get_response_page(category_url, i, session)) for i in
+                     range(2, n + 1)]  # (2, n + 1)]
+        for future in asyncio.as_completed(task_list):
+            response_page_list.append(await future)
+
+        time.sleep(0.1)
+        # await asyncio.sleep(0.1)
     return response_page_list
 
 
@@ -62,8 +69,9 @@ def get_urls(response_page):
         if len(items):
             for item in items:
                 product = item.select('a')
-                url = f"https://viyar.ua{product[0].get('href')}"
-                url_list.append(url[:-2])
+                url = product[0].get('href')
+                # url = f"https://viyar.ua{product[0].get('href')}"
+                url_list.append(url[:-1])
     except Exception as ex:
         print(f'!------------------{ex}------------------!')
     return url_list
@@ -83,32 +91,69 @@ def get_items_urls(response_page_list):
 #######################################################################################
 async def get_item_response(item_url, session):
     """Возвращает response обьект на product_detail"""
+    # try:
+    print('dddddddddd')
+
     async with session.get(item_url) as response:
+        print("ssssssssssss")
         return await response.text()
+    # except Exception as ex:
+    #     print(ex)
 
-
-async def get_items_responses(items_url_list):
+async def get_items_responses(items_url_list, session):
     """Возвращает список response обьектов на все товары внутри категории"""
     items_response_list = []
-    async with aiohttp.ClientSession() as session:
-        task_list = [asyncio.create_task(get_response_page(get_item_response, item_url, session)) for item_url in items_url_list]
-        for future in asyncio.as_completed(task_list):
-            items_response_list.append(await future)
+    # try:
+    print('get_items_responses')
+    task_list = [asyncio.create_task(get_item_response(item_url, session)) for item_url in items_url_list[:1]]
+    for future in asyncio.as_completed(task_list):
+        items_response_list.append(await future)
+
+    # except Exception as ex:
+    #     print(ex)
     await asyncio.sleep(0.1)
     return items_response_list
 
 
-if __name__ == '__main__':
-    start = time.time()
-    response_page_list = asyncio.run(get_response_pages('https://viyar.ua/catalog/yashchiki_mullerbox/'))
-    print(time.time() - start)
-    start = time.time()
-    items_url_list = get_items_urls(response_page_list)
-    print(time.time() - start)
+#######################################################################################
+async def main():
+    async with aiohttp.ClientSession() as session:
+        start = time.time()
+        response_page_list = await get_response_pages('https://viyar.ua/catalog/yashchiki_mullerbox', session)
 
-    start = time.time()
-    print(f'amount of responses {asyncio.run(get_items_responses(items_url_list))}')
-    print(time.time() - start)
+        print(time.time() - start)
+        start = time.time()
+        items_url_list = get_items_urls(response_page_list)
+        # print(items_url_list[1])
+        # time.sleep(5)
+        print(time.time() - start)
+        time.sleep(5)
+
+        start = time.time()
+        items_response_list = await get_items_responses(items_url_list, session)
+        print(f'amount of responses {len(items_response_list)}')
+        print(items_response_list[0])
+        print(time.time() - start)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+# async def main():
+#     start = time.time()
+#     response_page_list = asyncio.run(get_response_pages('https://viyar.ua/catalog/yashchiki_mullerbox'))
+#
+#     print(time.time() - start)
+#     start = time.time()
+#     items_url_list = get_items_urls(response_page_list)
+#     # print(items_url_list[1])
+#     # time.sleep(5)
+#     print(time.time() - start)
+#     time.sleep(5)
+#
+#     start = time.time()
+#     items_response_list = asyncio.run(get_items_responses(items_url_list))
+#     print(f'amount of responses {len(items_response_list)}')
+#     print(items_response_list[0])
+#     print(time.time() - start)
 
     # start = time.time()
     # print(get_response_pages1('https://viyar.ua/catalog/yashchiki_mullerbox/'))
